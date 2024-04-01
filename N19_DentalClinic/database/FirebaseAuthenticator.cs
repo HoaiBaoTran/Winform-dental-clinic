3563bd8e97c3070a97436719151bab3d31769649
HoaiBaoTran <hoaibaotran1104@gmail.com>
2024-04-01 23:23:42 +0700

    Create FirebaseAuthenticator

---
+++ N19_DentalClinic/database/FirebaseAuthenticator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N19_DentalClinic.database
+{
+    internal class FirebaseAuthenticator
+    {
+        private const string FirebaseApiKey = "YOUR_FIREBASE_API_KEY";
+    }
+}
