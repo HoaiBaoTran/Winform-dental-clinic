b3e132c86644ff7e5dbdf48310c0c51bd0014c51
TaTriet1412 <138562042+TaTriet1412@users.noreply.github.com>
2024-04-10 13:19:57 +0700

    Create Cons.cs

---
+++ N19_DentalClinic/DTO/Cons.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N19_ProjectForm.DAO
+{
+    class Cons
+    {
+        public static int DayOfWeek = 7;
+        public static int DayofColumn = 6;
+        public static int dateButtonWidth = 75;
+        public static int dateButtonHeight = 42;
+        public static int margin = 4;
+    }
+}
